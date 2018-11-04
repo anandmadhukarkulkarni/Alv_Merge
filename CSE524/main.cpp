b92b566ff1d3857d9628b7e595d7d2f92ad3f892
Anand Kulkarni <anakulkarni@cs.stonybrook.edu>
2018-11-03 23:41:48 -0400

    Initial Commit

---
+++ CSE524/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE524
+//
+//  Created by Anand Kulkarni on 11/3/18.
+//  Copyright © 2018 Anand Kulkarni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
